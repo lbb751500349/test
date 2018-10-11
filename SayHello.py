ee900936d06c52233fcf6216cf9a66e8ee2ee14a
unknown <baobao.lu@taimei.com>
2018-10-11 15:11:27 +0800

    first commit

---
+++ SayHello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding:utf-8
+
+"""
+@Author: Baob
+@Project:untitled1
+@software: PyCharm
+@file: test.py
+@time: 2018/10/11 14:02
+"""
+
+
+def Say_hello(people):
+    print('hello' + ',' + people)
+
+
+if __name__ == "__main__":
+    Say_hello('Tom')
